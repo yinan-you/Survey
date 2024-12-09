4a03a189fcb137cf4054000d62b96a9278804d82
yinan-you <136161039+yinan-you@users.noreply.github.com>
2024-12-09 13:29:12 +1100

    Moved folder

--- webapp/website/eoi.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-eoi = Blueprint('eoi', __name__)
-
-
-@eoi.route('/sign-up')
-def sign_up():
-    return "<p>Sign Up<p>"
--- webapp/website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,render_template
-
-views = Blueprint('views', __name__)
-
-@views.route('/') # whenever we go to / route, we will run this. This is the homepage
-def home():
-    return render_template("home.html")
