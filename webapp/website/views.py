10d02b24bc29bbe236769d5ad7cb7a12bd78aefb
yinan-you <136161039+yinan-you@users.noreply.github.com>
2024-12-09 13:27:22 +1100

    Debugging

--- webapp/website/views.py
+++ webapp/website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # whenever we go to / route, we will run this. This is the homepage
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
